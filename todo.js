bbf3e1cdf9c6c2dc711538e2c5e97fe7bad04250
karslio <karslio@users.noreply.github.com>
2019-04-06 11:37:32 +0200

    filtreleme

--- todo.js
+++ todo.js
@@ -54,19 +54,19 @@
 //  link.href ="#";
 //  link.className = "delete-item";
 //  link.innerHTML = "<i class = 'fa fa-remove' ></i>";
- 
+
 //  listItem.className = "list-group-item d-flex justify-content-between";
- 
+
 //  // text Node olusturma
- 
+
 //  listItem.appendChild(document.createTextNode(newTodo));
 //  listItem.appendChild(link);
- 
- 
+
+
 //  // todo List e List  İtem ı ekleme 
- 
+
 //  todoList.appendChild(listItem);
- 
+
 //  todoInput.value = "";
 // }
 
@@ -143,30 +143,119 @@
 //         while(todoList.firstElementChild != null){
 //             todoList.removeChild(todoList.firstElementChild);
 //         }
-    
+
 // }
 
-// console.log("merhaba");
 
 
-// // git add README.md
-// // git commit -m "first commit"
-// // git remote add origin https://github.com/karslio/listeleme-.git
-// // git push -u origin master */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 const form = document.querySelector("#todo-form");
 const todoInput = document.querySelector("#todo");
-const todoList = document.querySelector("#list-group");
-const firstCardBody = document.querySelectorAll(".card-bodoy")[0];
-const secondCardBody = document.querySelectorAll(".card-bodoy")[1];
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clearButton = document.querySelector("#clear-todos");
 
 
+eventListeners();
+
+function eventListeners() {
+    form.addEventListener("submit", addTodo);
+    secondCardBody.addEventListener("click", deleteTodo);
+    filter.addEventListener("keyup", find);
+}
+
+function dongu(e){
+    
+}
 
 
 
+function find(e) {
+    const filterValue = e.target.value.trim();
+
+    const listname = document.querySelectorAll(".list-group-item");
+
+    listname.forEach(function(e){
+        if (e.textContent.indexOf(filterValue)  === -1 ){
+            e.setAttribute("style", "display: none !important" )
+        }
+        else{
+            e.setAttribute("style", "display: block " )
+        }
+
+    })
+    
+
+}
+
+
+function addTodo(e) {
+
+    const newTodo = todoInput.value.trim();
+
+    if (newTodo === "") {
+
+    }
+    else {
+        addTodoUI(newTodo);
+    }
+    e.preventDefault();
+}
+
+
+function addTodoUI(newTodo) {
+    // < li class="list-group-item d-flex justify-content-between" >
+    //     Todo 1
+    //         < a href="#" class="delete-item" >
+    //         <i class="fa fa-remove"></i>
+    //     </a >
+
+    // </li >
+
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between";
+
+    const link = document.createElement("a");
+    link.href = "#";
+    link.innerHTML = "<i class='fa fa-remove'></i>";
+    link.className = "delete-item";
+
+    listItem.appendChild(document.createTextNode(newTodo));
+    listItem.appendChild(link);
+
+    todoList.appendChild(listItem);
+
+    todoInput.value = "";
+
+
+}
+
+function deleteTodo(e) {
+
+    console.log(e.target);
+
+    if (e.target.className === "fa fa-remove") {
+        e.target.parentElement.parentElement.remove();
+    }
+
+}
 
